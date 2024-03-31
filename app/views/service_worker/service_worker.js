d098559364eb30c02eb6fc69da51567075dc019e
Myroslav Martynov <marty.mircha@gmail.com>
2024-03-31 17:42:18 +0200

    feat: 🎸 register routes for offline fallback via workbox

--- app/views/service_worker/service_worker.js
+++ app/views/service_worker/service_worker.js
@@ -17,3 +17,37 @@ function onFetch(event) {
 self.addEventListener('install', onInstall)
 self.addEventListener('activate', onActivate)
 self.addEventListener('fetch', onFetch)
+
+// eslint-disable-next-line no-undef
+const { CacheFirst, NetworkFirst } = workbox.strategies
+// eslint-disable-next-line no-undef
+const { registerRoute } = workbox.routing
+// If we have critical pages that won't be changing very often, it's a good idea to use cache first with them
+registerRoute(
+  ({ url }) => url.pathname.startsWith('/home'),
+  new CacheFirst({
+    cacheName: 'documents',
+  }),
+)
+// For every other page we use network first to ensure the most up-to-date resources
+registerRoute(
+  ({ request, url }) =>
+    request.destination === 'document' || request.destination === '',
+  new NetworkFirst({
+    cacheName: 'documents',
+  }),
+)
+// For assets (scripts and images), we use cache first
+registerRoute(
+  ({ request }) =>
+    request.destination === 'script' || request.destination === 'style',
+  new CacheFirst({
+    cacheName: 'assets-styles-and-scripts',
+  }),
+)
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'assets-images',
+  }),
+)
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root 'home#index'
+  # root 'home#index'
+  get '/', to: redirect('/home/index')
   get "/service-worker.js" => "service_worker#service_worker"
   get "/manifest.json" => "service_worker#manifest"
 end
